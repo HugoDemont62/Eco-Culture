c0b7064dadb06c25ad9e2cd90ffab2807243685b
HugoDemont <hugodemont62@hotmail.fr>
2022-11-02 16:54:52 +0100

    Init App 1.03

--- app/src/main/java/com/example/ecoculture/MainActivity.kt
+++ app/src/main/java/com/example/ecoculture/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ecoculture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.accueil)
+        setContentView(R.layout.charging)
+
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            setContentView(R.layout.main_page)
+        }, 3000)
     }
 }
